d20d30036dbd7602e9ec774cfc5acd1286ca1ec5
Amanda Pedrazzoli <apedrazzoli@gmail.com>
2022-01-23 21:53:35 -0300

    Desafio 7.1 - Função para média

---
+++ 7_1_funcao_calc_media.py
@@ -0,0 +1,10 @@
+# Função para cálculo de média onde o usuário informa os valores das notas.
+
+def calcular_media():
+    nota1 = float(input('Digite a primeita nota: '))
+    nota2 = float(input('Digite a segunda nota: '))
+    media = (nota1 + nota2) / 2
+    return media
+
+minha_media = calcular_media()
+print('Minha média é: ', minha_media)
